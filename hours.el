c5888d11782a696789adc19232249eb7dcad7766
Jim Radford <radford@blackbean.org>
2003-11-20 20:18:04 -0800

    Multiple changes:
    - Add copyright
    - Better regular expressions (wrap fundamentals with word boundary assertions)
    - Add error checking to font-lock
    - Tweak some font-lock colors
    - Add hours-invoice to generate invoices
    - Provide hours.

--- hours.el
+++ hours.el
@@ -1,37 +1,60 @@
-(setq hours-time  "\\([0-9][0-9]?:[0-9][0-9]\\(?:am\\|pm\\)\\)"
-      hours-date  "\\(20[0-9][0-9]/[0-9][0-9]?/[0-9][0-9]?\\)"
-      hours-day   "\\(Sun\\|Mon\\|Tue\\|Wed\\|Thu\\|Fri\\|Sat\\)"
-      hours-hours "\\([0-9]+\\(?:\\.[0-9]+\\)?\\)")
-(setq hours-entry
-      (concat "^" hours-date " +" hours-day " +" hours-time " +- +" hours-time " +" hours-hours "$"))
-(setq hours-invoice
-      (concat "^Invoice +" hours-date " +" hours-day " +" hours-hours " +Hours"))
-(setq hours-outline-regexp
-      (concat "\\(" hours-invoice "\\|" hours-entry "\\)"))
+;; Copyright 2003 Jim Radford
 
-(setq hours-test "^jim")
+(setq hours-time  "\\<\\([0-9][0-9]?:[0-9][0-9]\\(?:am\\|pm\\)\\)\\>"
+      hours-date  "\\<\\(\\(20[0-9][0-9]\\)[-/]\\([0-9][0-9]\\)?[-/]\\([0-9][0-9]?\\)\\)\\>"
+      hours-day   "\\<\\(Sun\\|Mon\\|Tue\\|Wed\\|Thu\\|Fri\\|Sat\\)\\>"
+      hours-hours "\\<\\([0-9]+\\(?:\\.[0-9]+\\)?\\)\\>")
+(setq hours-interval
+      (concat hours-time " +->? +" hours-time " +" hours-hours))
+(setq hours-date-day
+      (concat hours-date " +" hours-day))
+(setq hours-outline-regexp
+      (concat "\\(^\\(?:Invoice +\\)?" hours-date "\\)"))
+
+(defun hours-check-date (date)
+  (hours-date-is-legal-p (hours-date-from-string date)))
+(defun hours-check-day (date day)
+  (setq date (hours-date-from-string date))
+  (and (hours-date-is-legal-p date)
+       (do-unless (equal day (calendar-day-name date 3))
+         (message "%s should be %s." day (calendar-day-name date 3)))))
+(defun hours-check-interval (time1 time2 hours)
+    (let* ((minutes (* (string-to-number hours) 60))
+           (interval (- (hours-time-in-minutes time2) (hours-time-in-minutes time1))))
+      (if (<= interval 0) (setq interval (+ interval (* 24 60))))
+      (do-unless (= interval minutes)
+        (message "%s should be %s." (/ (float minutes) 60) (/ (float interval) 60)))))
+
 (setq hours-font-lock-keywords
-      `(("hello" . font-lock-warning-face)
-        (,hours-entry   . (let ((date (hours-date-at-point))
-                                (day (string-match 2))
-                                (time1 (string-match 3))
-                                (time2 (string-match 4))
-                                (hours (string-match 5)))
-                            (if (and
-                                 (calendar-date-is-legal-p date)
-                                 (eq (calendar-day-name date 3) day)
-                                 (= (- time2 time1) hours))
-                                hours-entry-face
-                              hours-entry-face)))
-        (,hours-invoice . hours-invoice-face)
+      `((,hours-date-day
+         (1 (if (save-match-data
+                  (hours-check-date (match-string 1)))
+                hours-interval-face
+              font-lock-warning-face))
+         (5 (if (hours-check-day (match-string 1) (match-string 5))
+                hours-interval-face
+              font-lock-warning-face)))
+        (,hours-interval
+         (1 hours-interval-face)
+         (2 hours-interval-face)
+         (3 (if (hours-check-interval (match-string 1) (match-string 2) (match-string 3))
+                hours-interval-face
+              font-lock-warning-face)))
+        ("^Invoice\\>\\|\\<Hours *$" . hours-invoice-face)
+        (,hours-date
+         (1 (if (hours-check-date (match-string 0))
+                hours-date-face
+              font-lock-warning-face)))
         (,hours-day     . hours-day-face)
         (,hours-time    . hours-time-face)))
 
-;; use font-lock-warning-face for bad dates
+(setq hours-font-lock-defaults
+      '(hours-font-lock-keywords t nil nil nil (font-lock-multiline . nil)))
 
 (easy-mmode-defmap hours-mode-map
-  `(("\C-cd" . hours-next-date)
-    ("\C-c\C-c" . hours-something))
+  `(("\C-cd" . hours-insert-current-date)
+    ("\C-ci" . hours-invoice)
+    ("\C-c`" . hours-next-error))
   "Keymap for `hours-mode'.")
 
 (define-derived-mode hours-mode text-mode "Hours"
@@ -39,27 +62,73 @@
   (set (make-local-variable 'font-lock-defaults) hours-font-lock-defaults)
   (set (make-local-variable 'outline-regexp) hours-outline-regexp))
 
+;; (define-minor-mode hours-minor-mode
+;;   "Hours mode for viewing/editing time cards in hours format.
+;; \\{hours-minor-mode-map}"
+;;   nil " Hours" nil)
 
-(define-minor-mode hours-minor-mode
-  "Hours mode for viewing/editing time cards in hours format.
-\\{hours-minor-mode-map}"
-  nil " Hours" nil)
+(defun hours-invoice () (interactive)
+  (let ((total 0) error-location (bound (point)))
+    (save-excursion
+      (re-search-backward "^Invoice\\> .*$")
+      (goto-char (1+ (match-end 0)))
+      (while  ; a line that doesn't start with a space should be an entry
+          (and (re-search-forward "^\\S " bound 'noerror)
+               (do-unless
+                   (and
+                    (goto-char (match-beginning 0))
+                    (looking-at (concat "\\(?:---\\+ +\\)?" hours-date-day " +" hours-interval))
+                    (save-match-data (hours-check-day (match-string 1) (match-string 5)))
+                    (save-match-data (hours-check-interval (match-string 6) (match-string 7) (match-string 8)))
+                    (goto-char (match-end 0)))
+                 (setq error-location (point))))
+        (setq total (+ total (string-to-number (match-string 8))))))
+    (when error-location
+      (goto-char error-location)
+      (error "Illegal entry found" ))
+    (insert "Invoice ") (hours-insert-current-date) (insert (format " %s Hours\n" total))))
 
+(defun hours-date-is-legal-p (date)
+  (and date (calendar-date-is-legal-p date)))
+
+(require 'diary-lib)
+(defun hours-time-in-minutes (string)
+  (let* ((military (diary-entry-time string))
+         (hours (/ military 100))
+         (mins  (% military 100)))
+    (+ (* hours 60) mins)))
+
+(defmacro do-unless (cond &rest body)
+  "Eval BODY forms if COND is nil and return cond" 
+  `(let ((p ,cond))
+    (unless p 
+      ,@body)
+    p))
+(put 'do-unless 'lisp-indent-function 1)
 
 (require 'calendar)
 
-(defun hours-date-at-point ()
-  (if (looking-at "\\([0-9][0-9][0-9][0-9]\\)/\\([0-9][0-9]?\\)/\\([0-9][0-9]?\\)")
-      (list (string-to-int (match-string 2))
-            (string-to-int (match-string 3))
-            (string-to-int (match-string 1)))))
+;; (defun hours-date-at-point ()
+;;   (if (looking-at hours-date)
+;;       (list (string-to-int (match-string 3))
+;;             (string-to-int (match-string 4))
+;;            (string-to-int (match-string 2)))))
+
+(defun hours-date-from-string (string)
+  (if (string-match hours-date string)
+      (list (string-to-int (match-string 3 string))
+            (string-to-int (match-string 4 string))
+            (string-to-int (match-string 2 string)))))
 
 (defun hours-format-date (date)
   (format "%d/%02d/%02d"
           (nth 2 date) (nth 0 date) (nth 1 date)))
 
-;;(calendar-date-is-legal-p '(10 10 2003))
 ;;(calendar-current-date)
+
+(defun hours-insert-current-date () (interactive)
+  (let ((today (calendar-current-date)))
+    (insert (hours-format-date today) " " (calendar-day-name today 3)) " "))
 
 (defun hours-next-date () 
   (interactive) 
@@ -70,11 +139,11 @@
     (insert-string (format "\n%s %s "
                            (hours-format-date next) (calendar-day-name next 3)))))
 
-(defface hours-entry-face
+(defface hours-interval-face
   '((t (:foreground "blue" :bold t)))
   "`hours-mode' face used to highlight complete entries."
   :group 'hours-mode)
-(defvar hours-entry-face 'hours-entry-face)
+(defvar hours-interval-face 'hours-interval-face)
 
 (defface hours-invoice-face
   '((t (:foreground "green" :bold t)))
@@ -89,16 +158,15 @@
 (defvar hours-date-face 'hours-date-face)
 
 (defface hours-day-face
-  '((t (:foreground "magenta" :bold t)))
+  '((t (:foreground "yellow" :bold t)))
   "`hours-mode' face used to highlight complete entries."
   :group 'hours-mode)
 (defvar hours-day-face 'hours-day-face)
 
 (defface hours-time-face
-  '((t (:foreground "magenta" :bold t)))
+  '((t (:foreground "brown" :bold t)))
   "`hours-mode' face used to highlight complete entries."
   :group 'hours-mode)
 (defvar hours-time-face 'hours-time-face)
 
-(setq hours-font-lock-defaults
-      '(hours-font-lock-keywords t nil nil nil (font-lock-multiline)))
+(provide 'hours)
